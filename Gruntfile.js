459167b7ecdb2e16af037237cce0b47354e36403
Diego Pamio <diego.pamio@intel.com>
2014-05-20 00:59:57 -0300

    Initial version.

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/<%= pkg.name %>.js', 'lib/utils.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-bump');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+  grunt.registerTask('bump', ['bump']);
+};
---
+++ README.md
@@ -0,0 +1,40 @@
+Angular Sails Bind
+==================
+
+An AngularJS service to bind Angular models with sailsjs backend models using socket.io.
+
+
+Installation:
+
+```shell
+bower install angular-sails-bind
+```
+
+Usage
+-----
+
+A small example:
+
+```javascript
+var app = angular.module("MyApp", ['ngSailsBind']);
+app.controller("ItemsCtrl", function ($scope, $sailsBind) {
+  // To easily add new items to the collection.
+  $scope.newItem = {};  
+  /** This will:
+  *     1. Add a "items" model to $scope. (pluralized)
+  *     2. Get the data from your http://<examplesite.com>/item thru sailsjs socket get.
+  *     3. Setup socket io so that, when something changes in the sailsjs backend, they will be reflected
+  *        in the angular "items" model.
+  *     4. Watch the "items" model for collection changes in angular (add and removal of items
+  *        and send them to the backend using socket.
+  $sailsBind("item", $scope);
+```
+
+```html
+<div ng-controller="ItemsCtrl">
+  <input ng-model="newItem.name"/><a href="" ng-click="items.push(newItem)"></a>Add New
+  <ul>
+    <li ng-repeat="item in items">{{item.name}} <a href="" ng-click="items.splice(items.indexOf(item), 1)">remove</a></li>
+  </ul>
+</div>
+```
