cafeb28f6e886bc46fc9b25b36fdcbd51ca0ad24
Diego Pamio <diego.pamio@intel.com>
2014-06-12 12:22:14 -0300

    Cleaned the code (jslint) config files. Removed unused libraries, removed iOS as testing target.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,150 +1,103 @@
 /*global module:false*/
 /*global process:false*/
 module.exports = function (grunt) {
-    var browsers = [{
-        browserName: "firefox",
-        version: "19",
-        platform: "XP"
-    }, {
-        browserName: "chrome",
-        platform: "XP"
-    }, {
-        browserName: "chrome",
-        platform: "linux"
-    }, {
-        browserName: "internet explorer",
-        platform: "WIN8",
-        version: "10"
-    }, {
-        browserName: "internet explorer",
-        platform: "VISTA",
-        version: "9"
-    }, {
-        browserName: "opera",
-        platform: "Windows 2008",
-        version: "12"
-    }];
-  // Project configuration.
-  grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    // Task configuration.
-    jsdoc : {
-      dist : {
-          src: ['lib/*.js', 'test/*.js'],
-          options: {
-              destination: 'doc'
-          }
-      }
-    },
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['lib/<%= pkg.name %>.js', 'lib/utils.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    bump: {
-        options: {
-            commitFiles: ['-a'],
-            files: ['package.json', 'bower.json'],
-            pushTo: 'origin'
+    'use strict';
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+        // Task configuration.
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: true
+            },
+            dist: {
+                src: ['lib/<%= pkg.name %>.js', 'lib/utils.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            dist: {
+                src: '<%= concat.dist.dest %>',
+                dest: 'dist/<%= pkg.name %>.min.js'
+            }
+        },
+        bump: {
+            options: {
+                commitFiles: ['-a'],
+                files: ['package.json', 'bower.json'],
+                pushTo: 'origin'
+            }
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: true,
+                boss: true,
+                eqnull: true,
+                globals: {}
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            }
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.config.js'
+            }
+        },
+        coveralls: {
+            options: {
+                debug: true,
+                coverage_dir: 'test/coverage',
+                dryRun: false,
+                force: true,
+                recursive: true
+            }
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile', 'karma']
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    base: "",
+                    port: 9999
+                }
+            }
         }
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        globals: {}
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      }
-    },
-      karma: {
-          unit: {
-              configFile: 'karma.config.js'
-          }
-      },
-      coveralls: {
-          options: {
-              debug: true,
-              coverage_dir: 'test/coverage',
-              dryRun: false,
-              force: true,
-              recursive: true
-          }
-      },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile','karma']
-      }
-    },
-      connect: {
-          server: {
-              options: {
-                  base: "",
-                  port: 9999
-              }
-          }
-      },
-      'saucelabs-mocha': {
-          all: {
-              options: {
-                  urls: ["http://127.0.0.1:9999/"],
-                  tunnelTimeout: 5,
-                  build: process.env.CI_BUILD_NUMBER || '1303',
-                  concurrency: 3,
-                  browsers: browsers,
-                  testname: "mocha tests",
-                  tags: ["master"]
-              }
-          }
-      }
-  });
+    });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-bump');
-  grunt.loadNpmTasks('grunt-jsdoc');
-  grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-karma-coveralls');
-  grunt.loadNpmTasks('grunt-docular');
-  grunt.loadNpmTasks('grunt-saucelabs');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-karma-coveralls');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
-  // Default task.
-  grunt.registerTask('default', ['connect', 'jshint', 'concat', 'uglify', 'karma', 'coveralls']);
-  grunt.registerTask('test', ["connect", "saucelabs-mocha",'coveralls']);
-  grunt.registerTask('release', ['default','bump']);
+    // Default task.
+    grunt.registerTask('default', ['connect', 'jshint', 'concat', 'uglify', 'karma', 'coveralls']);
+    grunt.registerTask('release', ['default', 'bump']);
 };
--- karma.config.js
+++ karma.config.js
@@ -1,7 +1,9 @@
+/*global module:false*/
 // Karma configuration
 // Generated on Thu May 29 2014 22:32:20 GMT-0300 (ART)
 
-module.exports = function(config) {
+module.exports = function (config) {
+    'use strict';
     var customLaunchers = {
         sl_chrome: {
             base: 'SauceLabs',
@@ -13,12 +15,6 @@ module.exports = function(config) {
             browserName: 'firefox',
             version: '27'
         },
-        sl_ios_safari: {
-            base: 'SauceLabs',
-            browserName: 'iphone',
-            platform: 'OS X 10.9',
-            version: '7.1'
-        },
         sl_ie_11: {
             base: 'SauceLabs',
             browserName: 'internet explorer',
@@ -28,90 +24,90 @@ module.exports = function(config) {
     };
     config.set({
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '',
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
 
-    // frameworks to use
-    frameworks: ['mocha', 'chai', 'chai-as-promised'],
+        // frameworks to use
+        frameworks: ['mocha', 'chai', 'chai-as-promised'],
 
-    plugins: [
-      'karma-mocha',
-      'karma-chrome-launcher',
-      'karma-junit-reporter',
-      'karma-chai',
-      'karma-chai-plugins',
-      'karma-coverage',
-      'karma-mocha-reporter',
-      'karma-htmlfile-reporter',
-      'karma-sauce-launcher'
-    ],
-    preprocessors : {
-        'lib/*.js': 'coverage'
-    },
-    // list of files / patterns to load in the browser
-    files: [
-        'lib/dependencies/angular/*.js',
-        'lib/dependencies/angular-mocks/*.js',
-        'test/mocks/*.js',
-        'lib/*.js',
-        'test/**/*Spec.js'
-    ],
+        plugins: [
+            'karma-mocha',
+            'karma-chrome-launcher',
+            'karma-junit-reporter',
+            'karma-chai',
+            'karma-chai-plugins',
+            'karma-coverage',
+            'karma-mocha-reporter',
+            'karma-htmlfile-reporter',
+            'karma-sauce-launcher'
+        ],
+        preprocessors: {
+            'lib/*.js': 'coverage'
+        },
+        // list of files / patterns to load in the browser
+        files: [
+            'lib/dependencies/angular/*.js',
+            'lib/dependencies/angular-mocks/*.js',
+            'test/mocks/*.js',
+            'lib/*.js',
+            'test/**/*Spec.js'
+        ],
 
-    // list of files to exclude
-    exclude: [
-      
-    ],
+        // list of files to exclude
+        exclude: [
 
-    sauceLabs: {
-        testName: 'Web App Unit Tests'
-    },
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['coverage', 'mocha', 'html', 'saucelabs'],
-    htmlReporter: {
-      outputFile: 'test/report/resuts.html'
-    },
-    coverageReporter : {
-        type : 'lcov',
-        dir : 'test/coverage'
-    },
+        ],
 
-    // web server port
-    port: 9876,
+        sauceLabs: {
+            testName: 'Web App Unit Tests'
+        },
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['coverage', 'mocha', 'html', 'saucelabs'],
+        htmlReporter: {
+            outputFile: 'test/report/resuts.html'
+        },
+        coverageReporter: {
+            type: 'lcov',
+            dir: 'test/coverage'
+        },
 
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera (has to be installed with `npm install karma-opera-launcher`)
-    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-    // - PhantomJS
-    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    customLaunchers: customLaunchers,
-    browsers: Object.keys(customLaunchers),
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
 
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera (has to be installed with `npm install karma-opera-launcher`)
+        // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+        // - PhantomJS
+        // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+        customLaunchers: customLaunchers,
+        browsers: Object.keys(customLaunchers),
 
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: true
-  });
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
+
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: true
+    });
 };
