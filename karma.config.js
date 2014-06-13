211d6178f40e3773fdae44eacdcaac5fc1a7fe14
Diego Pamio <diego.pamio@intel.com>
2014-06-13 00:44:38 -0300

    Huge typo, _ instead of .

--- karma.config.js
+++ karma.config.js
@@ -5,7 +5,7 @@
 
 module.exports = function (config) {
     'use strict';
-    var environment = process.env_NODE_ENV || 'dev',
+    var environment = process.env.NODE_ENV || 'dev',
         customLaunchersByEnvironment = {
             test: {
                 sl_chrome: {
